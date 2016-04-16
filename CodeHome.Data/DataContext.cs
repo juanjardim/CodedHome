53c56a8dda17b2f5ee0b93fcbd28016ed50fd0cb
juan jardim <juanjardim@gmail.com>
2016-04-16 13:15:26 +0200

    Refactoring missing code

---
+++ CodeHome.Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodedHome.Model;
+
+namespace CodeHome.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() :base(DataContext.ConnectionStringName) {}
+        public DbSet<Home> Homes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public static string ConnectionStringName
+        {
+            get
+            {
+                if (ConfigurationManager.AppSettings["ConnectionStringName"] != null)
+                {
+                    return ConfigurationManager.AppSettings["ConnectionStringName"];
+                }
+                return "DefaultConnection";
+            }
+        }
+    }
+}
--- CodedHome.Model/User.cs
+++ CodedHome.Model/User.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace CodedHome.Model
 {
-    class User
+    public class User
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
     }
 }
