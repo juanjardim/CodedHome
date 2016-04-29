29d58efb1e6af5ac36b73e60bc71e69e62aaf202
Juan Jardim <juan.jardim@imd.org>
2016-04-29 11:40:30 +0200

    Add new Models to represent the Membership, OAuthMembership and Role. Change the User model to has the list of roles

---
+++ CodedHome.Model/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CodedHome.Model
+{
+    public class Role
+    {
+        public Role()
+        {
+            Users = new List<User>();
+        }
+
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
--- CodedHome.Model/User.cs
+++ CodedHome.Model/User.cs
@@ -8,9 +8,14 @@ namespace CodedHome.Model
 {
     public class User
     {
+        public User()
+        {
+            Roles = new List<Role>();
+        }
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
+        public ICollection<Role> Roles { get; set; }
     }
 }
